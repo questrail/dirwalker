18e4c36b9f33ac41a86ba1bb0fed96b8bbd2eb2c
Matthew Rankin <matthew.d.rankin@gmail.com>
2014-01-10 12:21:09 -0600

    Added comments and copyright info.

--- dirwalker.py
+++ dirwalker.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python
-# encoding: utf-8
-"""
-untitled.py
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 The dirwalker developers. All rights reserved.
+# Project site: https://github.com/questrail/applyaf
+# Use of this source code is governed by a MIT-style license that
+# can be found in the LICENSE.txt file for the project.
+"""Walk a directory searching for certain file extensions.
 
-Created by Matthew Rankin on 2010-11-16.
-Copyright (c) 2010 Matthew Rankin. All rights reserved.
+This package will walk multiple-level directories searching for files with the
+given extensions.
 """
 
+# Try to future proof code so that it's Python 3.x ready
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+
+# Standard module imports
 import os
 
 def find_filenames_with_extensions(
         search_directory, extensions):
+    """Find filenames with given extensions.
+
+    Args:
+        search_directory: A string of the absolute or relative directory to
+            search.
+        extensions: A list or tuple of strings containing the extensions to
+            look for.
+
+    Returns:
+        A list of filenames found.
+    """
     files_found = []
     for root, dirs, files in os.walk(search_directory):
         for filename in files:
